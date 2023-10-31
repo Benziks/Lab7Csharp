f5e9d31e4d1ad1aab13526aec0459c58423f93b3
Benziks <joker.nytik@gmail.com>
2023-10-31 17:55:01 +0200

    Добавьте файлы проекта.

---
+++ Lab7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+
+namespace Lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UniqueElements uniqueElements = new UniqueElements();
+
+            while (true)
+            {
+                Console.WriteLine("Введите элемент (или 'exit' для выхода): ");
+                string input = Console.ReadLine();
+                input = input.ToLower();
+    
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+                uniqueElements.AddInput(input);
+               
+            }
+
+            Console.WriteLine(uniqueElements);
+
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("\nВведите элемент для удаления ");
+            string searchElement = Console.ReadLine();
+            searchElement = searchElement.ToLower();
+
+            if (uniqueElements.RemoveElement(searchElement))
+            {
+                Console.WriteLine($"\"{searchElement}\" удален из коллекции.\n");
+            }
+            else
+            {
+                Console.WriteLine($"\"{searchElement}\" не найден для удаления.\n");
+            }
+
+            Console.WriteLine(uniqueElements);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
